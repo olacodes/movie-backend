20a309610844660ff6deb41f9a1be385dcc20a29
olacodes <olatundesodiq@gmail.com>
2019-11-29 16:02:16 +0100

    add comment and modularised the test data

--- movie_backend/movie_api/tests.py
+++ movie_backend/movie_api/tests.py
@@ -4,64 +4,55 @@ from rest_framework import status
 from .models import Movie
 import json
 from django.urls import reverse
-# from rest_framework.test import APIRequestFactory
-# Create your tests here.
-
+from mockData.data import MockData
 
 class MovieTest(APITestCase):
-
+    """
+    This method test the post resquest method and assert that the 
+    HTTP response status is HTTP_201_CREATED 
+    """
     def test_post(self):
-        data = {
-            "genre": "drama",
-            "title": "last blood",
-            "link" : "https://www.youtube.com/watch?v=4vWg5yJuWfs",
-            "image": "https://en.wikipedia.org/wiki/Rambo:_Last_Blood#/media/File:Rambo_-_Last_Blood_official_theatrical_poster.jpg",
-            "detail": "syvester stallon movie"
-        }
         client = APIClient()
         url = '/movies/movie/'
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(url, MockData.data_post(), format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 class GetMovie(APITestCase):
     url = '/movies/movie/1/'
-
     def setUp(self):
-        self.data = Movie.objects.create(
-            id = 1,
-            genre = "drama",
-            title = "last blood",
-            link = "https://www.youtube.com/watch?v=4vWg5yJuWfs",
-            image = "https://en.wikipedia.org/wiki/Rambo:_Last_Blood#/media/File:Rambo_-_Last_Blood_official_theatrical_poster.jpg",
-            detail = "syvester stallon movie"
-        )
-    def test_get(self):
-        print(self.url)
-        # url = '/movies/movie/'
-        response = self.client.get(self.url)
-        print(response)
+        self.data = Movie.objects.create(**MockData.set_up())
     
+    """
+    This method test the get resquest method and assert that the 
+    HTTP response status is HTTP_200_OK 
+    """
+    def test_get(self):
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+
+    """
+    This method test the get a movie resquest method and assert that the 
+    HTTP response status is HTTP_200_OK 
+    """
+
     def test_get_one_movie(self):
         response = self.client.get(self.url)
-        # print(Movie.objects.get(id=1))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    """
+    This method test the UPDATE resquest method and assert that the 
+    HTTP response status is HTTP_200_OK and  also check the response data for change
+    """
     def test_update_movie(self):
-        
-        data = {
-            "pk":1,
-            "genre":"action",
-            "title" :"last blood",
-            "link" : "https://www.youtube.com/watch?v=4vWg5yJuWfs",
-            "image" : "https://en.wikipedia.org/wiki/Rambo:_Last_Blood#/media/File:Rambo_-_Last_Blood_official_theatrical_poster.jpg",
-            "detail" : "syvester stallon movie"
-        }
-        url_one ='/movies/movie/1/' 
-        response = self.client.put(url_one, data)
+
+        response = self.client.put(self.url, MockData.data_update())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['genre'], 'action')
+
+    """
+    This method test the DELETE resquest method and assert that the 
+    HTTP response status is HTTP_204_NO_CONTENT
+    """
 
     def test_delete_movie(self):
         response = self.client.delete(self.url)
