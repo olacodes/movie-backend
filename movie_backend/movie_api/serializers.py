f75b842db9c9ca0cdd27d5651d9993dc10223813
olacodes <olatundesodiq@gmail.com>
2019-11-29 16:05:32 +0100

    add comment

--- movie_backend/movie_api/serializers.py
+++ movie_backend/movie_api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Movie
 
+"""
+The serializer class seriliaze and deserialize the data
+"""
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
